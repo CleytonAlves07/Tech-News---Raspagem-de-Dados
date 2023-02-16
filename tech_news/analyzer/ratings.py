c56778790acd4acdcd9e7274438b85f6606656d8
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-16 17:58:02 -0300

    feat: :construction: Função top_5_categories implementada
    Essa função mostra um rank com as 5 categorias que mais aparecem no blog

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,26 @@
-# Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list_of_news = find_news()
+    count_category = dict()
+
+    for new in list_of_news:
+        if new["category"] in count_category.keys():
+            count_category[new["category"]] += 1
+        else:
+            count_category[new["category"]] = 1
+
+    list_of_categories = [
+        ({"category": category, "amount": count_category[category]})
+        for category in count_category
+    ]
+
+    list_of_categories.sort(
+        key=lambda item: (-item["amount"], item["category"])
+    )
+    top5_categories = [
+        category["category"] for category in list_of_categories[:5]
+    ]
+
+    return top5_categories
